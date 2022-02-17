c505c64adb66037bf6bb44662f5ca0190876e141
pierrefittel <landrypierrefitte@gmail.com>
2022-02-18 03:14:45 +0900

    Added resolution, color picker, dark/light mode, etc.

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
-function createGrid(height, width, element) {
+function createGrid(resolution) {
     let row = '<div class="row">';
-    for (let i = 0; i < width; i++) {
-        row += element;
+    for (let i = 0; i < resolution; i++) {
+        row += '<div class="pixel"></div>';
     }
     row += '</div>';
     let grid = '';
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < resolution; i++) {
         grid += row;
     }
     return(grid);
 }
 
-function addColor(elements, color) {
+function addPixelEvent(elements) {
     elements.forEach(element => {
-        element.addEventListener("mouseover",function (e) {
-            e.target.style.backgroundColor = color;
-        });
+        element.addEventListener("mouseover", changeColor)
+        element.addEventListener("mousedown", changeColor)
     });
 }
 
-//Grid creation from createGrid
-const grid = document.getElementById("grid");
-const newGrid = createGrid(64, 64, '<div class="pixel"></div>');
-grid.innerHTML = newGrid;
+function changeColor(e) {
+    if (mouseDown == true) {
+        e.target.style.backgroundColor = color;
+    }
+}
 
-//Pixel color event handler
-const pixels = document.querySelectorAll(".pixel");
-pixels.onmousedown = addColor(pixels, "black");
+//Grid creation from createGrid
+let resolution = 128;
+let canvas = document.getElementById("canvas");
+let newGrid = createGrid(resolution);
+canvas.innerHTML = newGrid;
+
+//Color picker handler
+let color = document.getElementById("color-wheel").value;
+let colorwheel = document.getElementById("color-wheel");
+colorwheel.addEventListener("input", function() {
+    color = colorwheel.value;
+})
+
+//"Brush" event handler
+let pixels = document.querySelectorAll(".pixel");
+addPixelEvent(pixels);
+
+//Called by changeColor to control if mouse is down
+let mouseDown = false;
+canvas.onmousedown = () => (mouseDown = true)
+canvas.onmouseup = () => (mouseDown = false)
+
+//Create a new document
+let newSketch = document.getElementById("new");
+newSketch.addEventListener("click", function() {
+    canvas.innerHTML = '';
+    newGrid = createGrid(resolution);
+    canvas.innerHTML = newGrid;
+    pixels = document.querySelectorAll(".pixel");
+    addPixelEvent(pixels);
+})
+
+//Resolution slider toggle
+const resToggle = document.getElementById("canvas-res");
+const slider = document.getElementById("res-slider");
+resToggle.addEventListener("click", function() {
+    if (slider.style.visibility === "hidden") {
+        slider.style.visibility = "visible";
+    } else {
+        slider.style.visibility = "hidden";
+    }
+})
+
+//Resolution slider handle
+const resSlider = document.getElementById("slider");
+resSlider.oninput = function() {
+    resolution = resSlider.value;
+    const resValue = document.getElementById("slider-value");
+    resValue.innerHTML = resolution + "<br>x<br>" + resolution;
+};
+
+//Toogle dark/light mode
+const darkToggle = document.getElementById("mode");
+darkToggle.addEventListener("click", function() {
+    let header = document.getElementsByClassName("header")[0];
+    header.classList.toggle("light-mode-menu");
+    let toolBox = document.getElementsByClassName("toolbox")[0];
+    toolBox.classList.toggle("light-mode-menu");
+    let background = document.getElementsByClassName("workspace")[0];
+    background.classList.toggle("light-mode-canvas");
+})
+
+//Save canvas as bitmap file (not implemented)
+//...
